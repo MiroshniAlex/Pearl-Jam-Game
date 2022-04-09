55edbcc7cbb3d911f5619668af47d25945b3d048
Evan Deng <evjdeng@ad.unc.edu>
2022-04-09 00:49:04 -0400

    Started defining methods for Player class

--- player.py
+++ player.py
@@ -5,13 +5,19 @@ from pygame.locals import *
 
 class Player:
     """Player movement, updating, rendering, speed, acceleration."""
-
-    # TODO init function (params are x_pos, y_pos, rect)
-
-    # TODO define jump
-    # Takes the y_pos and changes the player object's speed thus changing pos
-
-    # TODO define move
-    # Takes x_pos and checks input in game.py file
     
-    # TODO define rendering function
+
+    def __init__(self, x_pos: float, y_pos: float, rect: pygame.Rect):
+        """Constructor definition for Player class!"""
+
+    # Takes the y_pos and changes the player object's speed thus changing pos
+    def jump(self, y_pos: float):
+        
+    
+
+    # Takes x_pos and checks input in game.py file
+    def move(self, x_pos: float):
+  
+    
+    # define rendering function
+    def
