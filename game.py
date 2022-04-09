26a75eafdf2f4edfb6b05f3e22c30a7d2e1d8199
Alexander Miroshnichenko <amiroshnichenko@unc.edu>
2022-04-08 23:54:06 -0400

    base skeleton code

--- game.py
+++ game.py
@@ -1,1 +1,36 @@
 """Main Game Running Space."""
+
+# Import Libraries
+import pygame
+from pygame.locals import *
+# import random if needed
+import sys
+
+# Import custom classes
+
+# Inintialize Pygame
+pygame.init()
+
+# Clock to control frame rate 
+clock = pygame.time.Clock()
+
+# TODO Instantiate Rooms
+
+# TODO Instantiate Player
+
+# Set up the drawing window and size
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+# Run the game until the user asks to quit or dies and `running` is set to False
+running = True
+
+# Game loop
+while running:
+    for event in pygame.event.get():
+        # Check for QUIT event (user closes window). If QUIT, then set running to false.
+        if event.type == QUIT:
+            running = False
+            pygame.quit()
+            sys.exit()
+        # TODO Check if user presses down on a key
+        
